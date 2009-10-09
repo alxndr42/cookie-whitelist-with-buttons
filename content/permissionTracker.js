72eea23b7db8c8d1f3eff43ba6147adc9770e264
Alexander Dietrich <alexander@dietrich.cx>
2009-10-09 20:21:41 +0200

    Interim state: permission cache removed, listeners and tracker integrated

--- content/permissionTracker.js
+++ content/permissionTracker.js
@@ -1,52 +1,43 @@
 // Copyright 2008 Alexander Dietrich <alexander@dietrich.cx>
 // Released under the terms of the GNU General Public License version 2 or later.
 
+function LOG(aMessage) {
+  const console =
+    Components.classes["@mozilla.org/consoleservice;1"].getService(
+      Components.interfaces.nsIConsoleService);
+  console.logStringMessage(aMessage);
+}
+
+function LOG_PROPS(aObject, aName) {
+  var result = "Properties of " + aName + ":\n";
+  for (var prop in aObject)
+    result += prop + " = " + aObject[prop] + "\n";
+  LOG(result);
+}
+
 const CWWBPermissionTracker = {
-  _pManager : undefined,
-  _pCache   : undefined,
+  NSIPERM : Components.interfaces.nsIPermission,
   
-  // Checks the current URI against permission cache or manager
+  _obsService : undefined,
+  _pManager : undefined,
+  _currentHost : undefined,
+  
+  // Checks the current URI's permission
   checkCurrentURI : function() {
     const uri  = gBrowser.currentURI;
     const host = this._filterHost(uri);
+    
+    if (host == this._currentHost)
+	  return;
+	
     var perm = null;
-    
-    if (host != null) {
-      if (this._pCache[host] == undefined) {
-        perm = this._pManager.testPermission(uri, "cookie");
-        this._pCache[host] = perm;
-      }
-      else {
-        perm = this._pCache[host];
-      }
-    }
+    if (host != null)
+{
+LOG("Test: " + host);
+      perm = this._pManager.testPermission(uri, "cookie");
+}
+    this._currentHost = host;
     CWWBAdd.updateState(host, perm);
-  },
-  
-  // Updates the permission cache when a tab is closed
-  tabClose : function(aURI) {
-    const tabHost = this._filterHost(aURI);
-    if (tabHost == null)
-      return;
-    
-    const browsers = gBrowser.browsers;
-    var count = 0;
-    for (var i = 0; i < browsers.length; i++) {
-      const host = this._filterHost(browsers[i].currentURI);
-      if (host == tabHost)
-        count++;
-    }
-    if (count == 1)
-      delete this._pCache[tabHost];
-  },
-  
-  // Resets the permission cache on whitelist edits
-  permissionChange : function(aChange, aHost, aPermission) {
-    if (aHost == null)
-      return;
-    
-    this._pCache = [];
-    this.checkCurrentURI();
   },
   
   // Returns the host of the given HTTP(S) URI, or null
@@ -66,21 +57,66 @@ const CWWBPermissionTracker = {
     return null;
   },
   
+  observe : function (aSubject, aTopic, aData) {
+    if (aTopic != "perm-changed")
+      return;
+    if (!(aSubject instanceof this.NSIPERM) || aSubject.type != "cookie")
+      return;
+    if (aSubject.host != this._currentHost)
+      return;
+    
+LOG("Permission change on current host");
+    this._currentHost = null;
+    this.checkCurrentURI();
+  },
+  
   init : function() {
     this._pManager =
       Components.classes["@mozilla.org/permissionmanager;1"].getService(
         Components.interfaces.nsIPermissionManager);
-    this._pCache = [];
+    this._currentHost = null;
     this.checkCurrentURI();
     
+    this._obsService =
+      Components.classes["@mozilla.org/observer-service;1"].getService(
+        Components.interfaces.nsIObserverService);
+    this._obsService.addObserver(this, "perm-changed", false);
     CWWBProgressListener.init();
-    CWWBTabListener.init();
-    CWWBPermissionListener.init();
   },
   
   cleanup : function() {
+    this._obsService.removeObserver(this, "perm-changed");
     CWWBProgressListener.cleanup();
-    CWWBTabListener.cleanup();
-    CWWBPermissionListener.cleanup();
   }
 };
+
+// Observes URI change events (page load, tab switch, etc.)
+const CWWBProgressListener = {
+  nsIWPL : Components.interfaces.nsIWebProgressListener,
+  nsISWR : Components.interfaces.nsISupportsWeakReference,
+  nsIS   : Components.interfaces.nsISupports,
+  
+  QueryInterface : function(aIID) {
+    if (
+      aIID.equals(this.nsIWPL) ||
+      aIID.equals(this.nsISWR) ||
+      aIID.equals(this.nsIS))
+        return this;
+    
+    throw Components.results.NS_NOINTERFACE;
+  },
+  
+  onLocationChange : function() {
+    CWWBPermissionTracker.checkCurrentURI();
+  },
+  
+  init : function() {
+    gBrowser.addProgressListener(
+      CWWBProgressListener,
+      Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+  },
+  
+  cleanup : function() {
+  }
+};
+
